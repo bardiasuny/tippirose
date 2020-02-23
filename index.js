a3e640f62a618627d9379b957d6c18c43460d824
bardia <bardiaheidari@bardia-2.home>
2020-02-23 10:50:44 +0000

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 var cors = require('cors');
+var path = require('path')
+
 const app = express()
 const port = process.env.PORT || 7000
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,6 +14,7 @@ const adminRoutes = require('./routes/adminRoutes')
 
 
 
+
 app.use('/admin', adminRoutes.routes)
 
 // app.use('/', (req, res, next) => {
@@ -19,6 +22,11 @@ app.use('/admin', adminRoutes.routes)
 // })
 
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('tippirose-app/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'tippirose-app', 'build', 'index.html'))
+    })
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
